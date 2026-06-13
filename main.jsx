fcb103d9e528d0c2e1508768387b5c45692a257b
aissaouijunaydja-coder <aissaoui.junayd.ja@gmail.com>
2026-06-13 19:15:45 +0200

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../contrat-freelance.jsx";
+import App from "./contrat-freelance.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
